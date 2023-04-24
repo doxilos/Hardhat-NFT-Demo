f16a477b978e3b210eb2b2b06ebe3ee6becd21a8
Nurbaki Vural <nurbakivural@gmail.com>
2023-04-24 17:26:20 +0300

    Added dynamic nft contract

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -83,7 +83,7 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
     // 0 - 99
     uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
     Breed dogBreed = getBreedFromModdedRng(moddedRng);
-    s_tokenCounter += s_tokenCounter;
+    s_tokenCounter += s_tokenCounter + 1;
 
     _safeMint(dogOwner, newTokenId);
     _setTokenURI(newTokenId, s_dogTokenUris[uint256(dogBreed)]);
